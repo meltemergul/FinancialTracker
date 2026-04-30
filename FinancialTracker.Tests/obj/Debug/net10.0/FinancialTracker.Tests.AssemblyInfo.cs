8cb5671743d765ee9103f730adc73fa8db80352e
Meltem Ergül <meltemergul34@gmail.com>
2026-04-30 15:53:16 +0300

    Testing issues have been resolved and the version has been downgraded

--- FinancialTracker.Tests/obj/Debug/net10.0/FinancialTracker.Tests.AssemblyInfo.cs
+++ FinancialTracker.Tests/obj/Debug/net10.0/FinancialTracker.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialTracker.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87207174bc96ad0db055d6b556b7ea5b73f0faf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9a7d9f06db210ddaa9fa5c0c430c1ab19a3c54")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialTracker.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialTracker.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
